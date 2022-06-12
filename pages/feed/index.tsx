46aba0e9a270b58b6999e957468df7139f3743ac
Dylan J <dylan@tritan.gg>
2022-06-12 06:02:20 -0400

    okkk

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -6,16 +6,22 @@ import Feed from '../../components/Feed/Feed';
 
 export default function viewFeed() {
     const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(`/api/posts/fetch?limit=50/`);
             const newData = await response.json();
             setData(newData);
+            setIsLoading(false);
         };
 
         fetchData();
     });
+
+    if (isLoading) {
+        return <Navbar />;
+    }
 
     return (
         <>
