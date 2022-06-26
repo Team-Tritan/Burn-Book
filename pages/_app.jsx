bb94df31e66906cc838f0dd2a80f5baa366fc88a
Dylan J <dylan@tritan.gg>
2022-06-26 06:06:17 -0400

    beta banner

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,19 @@ function BurnBook({ Component, pageProps }) {
     return (
         <SSRProvider>
             <Navbar />
+            <div className="d-flex justify-content-md-center">
+                <div
+                    class="alert alert alert-dismissible show text-center"
+                    style={{ backgroundColor: '#ff8da1', color: 'white' }}
+                    role="alert"
+                >
+                    <strong>We are currently in a very early beta!</strong> Please join our{' '}
+                    <a style={{ textDecoration: 'none' }} href="https://discord.gg/http">
+                        discord server
+                    </a>{' '}
+                    for feedback or support.
+                </div>
+            </div>
             <Component {...pageProps} />
         </SSRProvider>
     );
